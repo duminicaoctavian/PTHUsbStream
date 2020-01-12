955a1e1a6fe28e1826e901accba99c8fae05ab81
DESKTOP-K5G7K9R\Octavian <duminicaoctavian@gmail.com>
2020-01-12 17:49:45 +0200

    PTH USB streaming

---
+++ source/AppController.c
@@ -0,0 +1,237 @@
+/*
+* Licensee agrees that the example code provided to Licensee has been developed and released by Bosch solely as an example to be used as a potential reference for application development by Licensee. 
+* Fitness and suitability of the example code for any use within application developed by Licensee need to be verified by Licensee on its own authority by taking appropriate state of the art actions and measures (e.g. by means of quality assurance measures).
+* Licensee shall be responsible for conducting the development of its applications as well as integration of parts of the example code into such applications, taking into account the state of the art of technology and any statutory regulations and provisions applicable for such applications. Compliance with the functional system requirements and testing there of (including validation of information/data security aspects and functional safety) and release shall be solely incumbent upon Licensee. 
+* For the avoidance of doubt, Licensee shall be responsible and fully liable for the applications and any distribution of such applications into the market.
+* 
+* 
+* Redistribution and use in source and binary forms, with or without 
+* modification, are permitted provided that the following conditions are 
+* met:
+* 
+*     (1) Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer. 
+* 
+*     (2) Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in
+*     the documentation and/or other materials provided with the
+*     distribution.  
+*     
+*     (3)The name of the author may not be used to
+*     endorse or promote products derived from this software without
+*     specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR 
+*  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+*  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+*  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+*  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+*  POSSIBILITY OF SUCH DAMAGE.
+*/
+/*----------------------------------------------------------------------------*/
+
+/**
+ * @ingroup APPS_LIST
+ *
+ * @defgroup STREAM_SENSOR_DATA_OVER_USB StreamSensorDataOverUsb
+ * @{
+ *
+ * @brief Application of printing all the defined sensors on serialport
+ *
+ * @details Demo application of printing all the defined sensors on serialport(USB virtual comport)
+ *          every configured interval (#APP_CONTROLLER_TX_DELAY)
+ *
+ * @file
+ **/
+
+/* module includes ********************************************************** */
+
+/* own header files */
+#include "XdkAppInfo.h"
+
+#undef BCDS_MODULE_ID  /* Module ID define before including Basics package*/
+#define BCDS_MODULE_ID XDK_APP_MODULE_ID_APP_CONTROLLER
+
+/* own header files */
+#include "AppController.h"
+
+/* system header files */
+#include <stdio.h>
+
+/* additional interface header files */
+#include "XDK_Sensor.h"
+#include "XDK_Utils.h"
+#include "BCDS_Assert.h"
+#include "BCDS_CmdProcessor.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* constant definitions ***************************************************** */
+
+/* local variables ********************************************************** */
+
+static CmdProcessor_T *AppCmdProcessor;/**< Handle to store the main Command processor handle to be reused by ServalPAL thread */
+
+static xTaskHandle AppControllerHandle = NULL;/**< OS thread handle for Application controller to be used by run-time blocking threads */
+
+/** Sensor setup parameters */
+static Sensor_Setup_T SensorSetup =
+        {
+                .CmdProcessorHandle = NULL,
+                .Enable =
+                        {
+                                .Accel = false,
+                                .Mag = false,
+                                .Gyro = false,
+                                .Humidity = true,
+                                .Temp = true,
+                                .Pressure = true,
+                                .Light = false,
+                                .Noise = false,
+                        },
+                .Config =
+                        {
+                                .Temp =
+                                        {
+                                                .OffsetCorrection = APP_TEMPERATURE_OFFSET_CORRECTION
+                                        },
+                        }
+        };
+
+/* global variables ********************************************************* */
+
+/* inline functions ********************************************************* */
+
+/* local functions ********************************************************** */
+
+/**
+ * @brief This function controls the application flow
+ * - Triggers Sensor data sampling
+ * - Read the sampled Sensor data
+ *
+ * @param[in] pvParameters
+ * Unused
+ */
+static void AppControllerFire(void* pvParameters)
+{
+    BCDS_UNUSED(pvParameters);
+
+    Retcode_T retcode = RETCODE_OK;
+    Sensor_Value_T sensorValue;
+
+    while (1)
+    {
+        memset(&sensorValue, 0x00, sizeof(sensorValue));
+
+        retcode = Sensor_GetData(&sensorValue);
+        if (RETCODE_OK == retcode)
+        {
+            if (SensorSetup.Enable.Pressure && SensorSetup.Enable.Temp && SensorSetup.Enable.Humidity)
+            {
+                printf("BME280 Environmental Data: { Pressure = %ld Pa, Temperature = %ld mDeg, rh = %ld %%rh }\n",
+                        (long int) sensorValue.Pressure, (long int) sensorValue.Temp, (long int) sensorValue.RH);
+            }
+        }
+        if (RETCODE_OK != retcode)
+        {
+            Retcode_RaiseError(retcode);
+        }
+        vTaskDelay(pdMS_TO_TICKS(APP_CONTROLLER_TX_DELAY));
+    }
+}
+
+/**
+ * @brief To enable the necessary modules for the application
+ * - Sensor
+ *
+ * @param[in] param1
+ * Unused
+ *
+ * @param[in] param2
+ * Unused
+ */
+static void AppControllerEnable(void * param1, uint32_t param2)
+{
+    BCDS_UNUSED(param1);
+    BCDS_UNUSED(param2);
+
+    Retcode_T retcode = Sensor_Enable();
+    if (RETCODE_OK == retcode)
+    {
+        if (pdPASS != xTaskCreate(AppControllerFire, (const char * const ) "AppController", TASK_STACK_SIZE_APP_CONTROLLER, NULL, TASK_PRIO_APP_CONTROLLER, &AppControllerHandle))
+        {
+            retcode = RETCODE(RETCODE_SEVERITY_ERROR, RETCODE_OUT_OF_RESOURCES);
+        }
+    }
+    if (RETCODE_OK != retcode)
+    {
+        printf("AppControllerEnable : Failed \r\n");
+        Retcode_RaiseError(retcode);
+        assert(0); /* To provide LED indication for the user */
+    }
+    Utils_PrintResetCause();
+}
+
+/**
+ * @brief To setup the necessary modules for the application
+ * - Sensor
+ *
+ * @param[in] param1
+ * Unused
+ *
+ * @param[in] param2
+ * Unused
+ */
+static void AppControllerSetup(void * param1, uint32_t param2)
+{
+    BCDS_UNUSED(param1);
+    BCDS_UNUSED(param2);
+
+    SensorSetup.CmdProcessorHandle = AppCmdProcessor;
+    Retcode_T retcode = Sensor_Setup(&SensorSetup);
+    if (RETCODE_OK == retcode)
+    {
+        retcode = CmdProcessor_Enqueue(AppCmdProcessor, AppControllerEnable, NULL, UINT32_C(0));
+    }
+    if (RETCODE_OK != retcode)
+    {
+        printf("AppControllerSetup : Failed \r\n");
+        Retcode_RaiseError(retcode);
+        assert(0); /* To provide LED indication for the user */
+    }
+}
+
+/* global functions ********************************************************** */
+
+/** Refer interface header for description */
+void AppController_Init(void * cmdProcessorHandle, uint32_t param2)
+{
+    BCDS_UNUSED(param2);
+
+    Retcode_T retcode = RETCODE_OK;
+
+    if (cmdProcessorHandle == NULL)
+    {
+        printf("AppController_Init : Command processor handle is NULL \r\n");
+        retcode = RETCODE(RETCODE_SEVERITY_ERROR, RETCODE_NULL_POINTER);
+    }
+    else
+    {
+        AppCmdProcessor = (CmdProcessor_T *) cmdProcessorHandle;
+        retcode = CmdProcessor_Enqueue(AppCmdProcessor, AppControllerSetup, NULL, UINT32_C(0));
+    }
+
+    if (RETCODE_OK != retcode)
+    {
+        Retcode_RaiseError(retcode);
+        assert(0); /* To provide LED indication for the user */
+    }
+}
+
+/**@} */
+/** ************************************************************************* */
